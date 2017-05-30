7c64e55f1a828bf4313ec7e12e62bfc7df1253a9
NoahatYu <noahpotash@gmail.com>
2017-05-30 19:35:51 -0400

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -9,10 +9,13 @@ class Board(object):
         print(my_board.board[1][0] + "|" + my_board.board[1][1] + "|" + my_board.board[1][2])
         print(my_board.board[2][0] + "|" + my_board.board[2][1] + "|" + my_board.board[2][2])
 
-    #Make this method
-    def Win():
+    #Make this method- (unfinished)
+    def win():
         #check for win
         boardWin = False
+    def is_valid_move():
+        pass
+
 
 # create board object
 my_board = Board()
@@ -31,25 +34,29 @@ def changeTurn(turn):
         turn = Turn.X
     elif(turn is Turn.X):
         turn = Turn.O
-    return turn
 
     # at the end of each turn print the board
     Board.printboard()
+    return turn
+
+
 
 # Main Method run the game
 def main():
     print("*****************Welcome to TicTacToe******************\n")
+
     #Create turns
     aTurn = Turn()
     current_turn = aTurn.X
-    while(Board.Win() != False):
+    while(Board.win() != False):
         input_row = input("Player " + current_turn +" Enter Row number: ")
         input_col = input("Player " + current_turn + " Enter Column number: ")
 
         #place piece on board
         my_board.board[int(input_row)][int(input_col)] = current_turn
+        #change turns
         current_turn = changeTurn(current_turn)
-        Board.printboard()
+
 
 
 
