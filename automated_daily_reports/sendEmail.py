fa0899b791f32a88fc7251dbe9a9ea016c05d04e
NoahatYu <noahpotash@gmail.com>
2017-07-06 12:13:28 -0400

    Update sendEmail.py

--- automated_daily_reports/sendEmail.py
+++ automated_daily_reports/sendEmail.py
@@ -6,7 +6,8 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 
-LKQD_dict = {"SelectMedia_MW_AUS_Android_$2_floor": "SelectMedia::Direct::Mobile::AUS-Android-2_DemandExtreme",
+class sendEmails: 
+    LKQD_dict = {"SelectMedia_MW_AUS_Android_$2_floor": "SelectMedia::Direct::Mobile::AUS-Android-2_DemandExtreme",
                 "SelectMedia_MW_BR-IOS_$0.5_floor": "SelectMedia::Direct::Mobile::BR-IOS-0.5_DemandExtreme",
                 "SelectMedia_MW_BR-IOS_$1_floor": "SelectMedia::Direct::Mobile::BR-IOS-1_DemandExtreme",
                "SelectMedia_MW_HK-Android_$1_floor": "SelectMedia::Direct::Mobile::HK-Android-1_DemandExtreme",
@@ -17,37 +18,33 @@ LKQD_dict = {"SelectMedia_MW_AUS_Android_$2_floor": "SelectMedia::Direct::Mobile
                 "SelectMedia_MW_SG-Android_$2_floor": "SelectMedia::Direct::Mobile::SG-Android-2_DemandExtreme",
                 "SelectMedia_MW_SG-IOS_$2_floor": "SelectMedia::Direct::Mobile::SG-IOS-2_DemandExtreme"}
 
-from_address = ""
-to_address = "daniel.t@taboola.com"
+    from_address = ""
+    to_address = ""
 
-#date_post = datetime.now() - timedelta(days=2)
-#date_post = date_post.strftime(("%B %d, %Y"))
 
-#nameOfFile = final_taboola + "_" + date_post
+    msg = MIMEMultipart()
 
-msg = MIMEMultipart()
+    msg['From'] = from_address
+    msg['To'] = to_address
+    msg['Subject'] = "" # SUBJECT OF THE EMAIL
 
-msg['From'] = from_address
-msg['To'] = to_address
-msg['Subject'] = "" # SUBJECT OF THE EMAIL
+    body = "" # TEXT YOU WANT TO SEND
 
-body = "" # TEXT YOU WANT TO SEND
+    msg.attach(MIMEText(body, 'plain'))
 
-msg.attach(MIMEText(body, 'plain'))
+    filename = ""     #"NAME OF THE FILE WITH ITS EXTENSION"
+    attachment = open("", "rb") # PATH OF THE FILE
 
-filename = ""     #"NAME OF THE FILE WITH ITS EXTENSION"
-attachment = open("", "rb") # PATH OF THE FILE
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
 
-part = MIMEBase('application', 'octet-stream')
-part.set_payload((attachment).read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    msg.attach(part)
 
-msg.attach(part)
-
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(from_address, "password")
-text = msg.as_string()
-server.sendmail(from_address, to_address, text)
-server.quit()
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(from_address, "password")
+    text = msg.as_string()
+    server.sendmail(from_address, to_address, text)
+    server.quit()
