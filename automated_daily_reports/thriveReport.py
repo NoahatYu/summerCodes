49ad66cbb4908ffbdd3b63fb2f312c7875f31af8
NoahatYu <noahpotash@gmail.com>
2017-07-10 16:42:55 -0400

    Update thriveReport.py

--- automated_daily_reports/thriveReport.py
+++ automated_daily_reports/thriveReport.py
@@ -12,7 +12,6 @@ from selenium.webdriver.support.ui import WebDriverWait  # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC  # available since 2.26.0
 class Thrive:
     # TODO: Make sure I check what day it is,because if it is a monday the weekend days must be done as well.
-
     # Monday is 0 and Sunday is 6
     DayOfTheWeek = datetime.today().weekday()
     # Get the current Date
@@ -33,11 +32,11 @@ class Thrive:
 
     loginPage = browser.get('https://video.springserve.com/')
 
-    username = browser.find_element_by_xpath("//*[@id='user_email']")
-    password = browser.find_element_by_xpath("//*[@id='user_password']")
+    username = browser.find_element_by_id("user_email")
+    password = browser.find_element_by_id("user_password")
 
-    usernameThrive = "eedo.b@taboola.com"
-    passwordThrive = "taboolaspring22!"
+    usernameThrive = ""
+    passwordThrive = ""
     # Type in username and password
     username.send_keys(usernameThrive)
     password.send_keys(passwordThrive)
@@ -49,13 +48,15 @@ class Thrive:
 
     reportPage = browser.get("https://video.springserve.com/reports")
 
+    a_year1 = str(the_time.year % 100)
     a_month1 = str(the_time.month)
     a_day1 = str(the_time.day - 2)
+    a_year2 = str(the_time.year % 100)
     a_month2 = str(the_time.month)
     a_day2 = str(the_time.day - 2)
 
-    # Get the report page
-    date_report_page = "https://video.springserve.com/reports?date_range=Custom&custom_date_range=" + a_month1 +"%2F" + a_day1+ "%2F17+00%3A00+-+" + a_month2 + "%2F" + a_day2 + "%2F17+23%3A00&interval=Day&timezone=America%2FNew_York&dimensions%5B%5D=supply_tag_id"
+    # Get the report page and run the report
+    date_report_page = "https://video.springserve.com/reports?date_range=Custom&custom_date_range=" + a_month1 +"%2F" + a_day1 + "%2F" + a_year1 + "+00%3A00+-+" + a_month2 + "%2F" + a_day2 + "%2F" + a_year2 + "+23%3A00&interval=Day&timezone=America%2FNew_York&dimensions%5B%5D=supply_tag_id"
     browser.get(date_report_page)
     runReportButton = browser.find_element_by_name("commit")
     runReportButton.click()
@@ -81,7 +82,7 @@ class Thrive:
     dict_Name = {
        "Taboola_MW_JS__Tier_1_ENG__2": "Thrive_Mobile_2", "Taboola_Desktop__320x180__SP_ENG__3": "Thrive_Direct_Desktop_5"
     }
-
+    sleep(3)
     for table_row in table_row_data:
         table_row = table_row.text
         current_row = table_row.split(" ")
