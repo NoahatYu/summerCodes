3d4ec7f0ae0740f09ce0ecaf31269c4e5d53a164
NoahatYu <noahpotash@gmail.com>
2017-08-07 10:22:53 -0400

    Update SekindoReport.py

--- automatedDailyReport -v2.0/SekindoReport.py
+++ automatedDailyReport -v2.0/SekindoReport.py
@@ -1,15 +1,9 @@
-import re
-import os
-import csv
 import time
-import logging
 from time import sleep
 from PulsePointReport import PulsePoint
 from datetime import datetime
 from datetime import timedelta
 from selenium import webdriver
-from bs4 import BeautifulSoup
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import ElementNotVisibleException
 from selenium.webdriver.chrome.options import Options
@@ -17,6 +11,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait  # available since 2.4.0
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC  # available since 2.26.0
+
+"""
+Author - Noah Potash 07/15/2017
+"""
 
 
 class Sekindo:
@@ -32,6 +30,7 @@ class Sekindo:
     end_date = date_1 - timedelta(days=2)
     end_date = end_date.strftime('%m/%d/%Y')
 
+
     # This one is in "Month name day, full year ex.July 12,2017
     date_post = datetime.now() - timedelta(days=2)
     date_post = date_post.strftime(("%B %d, %Y"))
@@ -39,15 +38,17 @@ class Sekindo:
     the_time = datetime.now()
 
     # Get dates to add to url to get report
-    a_year = str(the_time.year)
-    a_month = str(the_time.month).zfill(2)
-    a_day = str(the_time.day - 2).zfill(2)
+
+    dayMonthYear = str(end_date).split("/")
+    a_month = dayMonthYear[0].zfill(2)
+    a_day = dayMonthYear[1].zfill(2)
+    a_year = dayMonthYear[2]
 
 
     the_time = datetime.now()
     location_of_file = "/Users/noah.p/Desktop/DailyReports/"
 
-    logFile = "/Users/noah.p/PycharmProjects/autoReports/DailyReportsLog/AutoDailyManualReportslogs.log/"
+    logFile = "/Users/noah.p/PycharmProjects/autoReports/DailyReportsDataLog/AutoDailyManualReportslogs.log/"
     logName = "Sekindo"
 
     dict_T = {
@@ -57,7 +58,7 @@ class Sekindo:
         "TaboolaENLPJS": "Sekindo_Direct_Tier1_LP_JS"
     }
 
-    def __init__(self, a_year,a_month,a_day, date_post, location_of_file, dict_T):
+    def __init__(self, a_year, a_month,a_day, date_post, location_of_file, dict_T):
         """
         Constructor
         :param end_date:
@@ -78,7 +79,6 @@ class Sekindo:
         :return the browser:
         """
         browser = webdriver.Firefox()
-        browser.maximize_window()
         browser.wait = WebDriverWait(browser, 5)
         return browser
 
@@ -159,10 +159,11 @@ class Sekindo:
                 dataBtn.click()
                 break
 
+        # finds the date box
         dates_cal = browser.find_elements_by_class_name("sekindoRedBorder")
         fromDay = dates_cal[0]
         toDay = dates_cal[1]
-        # Format is yyyy-day-month
+        # Format is yyyy-dd-mm
         fromDay.send_keys(a_year + "-" + a_month + "-" + a_day)
         toDay.send_keys(a_year + "-" + a_month + "-" + a_day)
 
@@ -178,10 +179,9 @@ class Sekindo:
             while len(loader) > 0:
                 loader = browser.wait.until(EC.visibility_of_all_elements_located((By.ID, "frotateG_01")))
                 sleep(6)
-
-
         except:
             try:
+                sleep(2)
                 # Find the reports data
                 # Once the exception is caught. It is know that the page is ready to be scraped
                 #sleep(6)
@@ -199,9 +199,7 @@ class Sekindo:
                     reportsPage = browser.find_element_by_tag_name("sekindo-quick-stats")
                     reportsPageData2 = reportsPage.text
                     reportsPageDataList = reportsPageData2.splitlines()
-
                     reportsPageData = reportsPage.text
-
 
                 except:
                     logger.error("Failed to find table data")
@@ -219,6 +217,7 @@ class Sekindo:
         imp_rev_list = data_list[8::2]
         the_length = len(imp_rev_list)
 
+        # The campaigns
         the_name_list = ["TaboolaUSLP","TaboolaUSLPJS", "TaboolaBRDTJS","TaboolaENLPJS"]
 
         # Loops through all data to single out impressions and revenue to correspond to name/domain/reportname
@@ -239,7 +238,7 @@ class Sekindo:
         sleep(6)
         # Scroll down to bottom of page
         browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        screenShot = browser.save_screenshot(filename="/Users/noah.p/PycharmProjects/autoReports/DailyReportsLog/SekindoData.png")
+        screenShot = browser.save_screenshot(filename="/Users/noah.p/PycharmProjects/autoReports/DailyReportsDataLog/SekindoData.png")
 
         return name_list2, imp_list, rev_list
 
@@ -255,11 +254,11 @@ def main():
     pp = PulsePoint(Sekindo.end_date, Sekindo.date_post, Sekindo.location_of_file,Sekindo.dict_T)
     browser = sk.start_browser()
     logger = pp.logToFile(browser, Sekindo.logFile, Sekindo.logName)
-    sk.lookup(browser,logger)
-    sk.fillInDateAndRunReport(browser, sk.a_year,sk.a_month,sk.a_day,logger)
+    sk.lookup(browser, logger)
+    sk.fillInDateAndRunReport(browser, sk.a_year,sk.a_month,sk.a_day, logger)
     name_list, imp_list, rev_list = sk.getData(browser, logger)
     if len(name_list) is 0 or len(imp_list) is 0 or len(rev_list) is 0:
-        name_list, imp_list, rev_list = sk.getData(browser,logger)
+        name_list, imp_list, rev_list = sk.getData(browser, logger)
 
     pp.makeCSV(sk.location_of_file, sk.dict_T, name_list, imp_list, rev_list, sk.end_date, sk.date_post)
     browser.quit()
